1c3509af283ea45172ea6dc0137d7d9f560fd8d8
Tawhid <tawhidumar.21@protonmail.com>
2024-03-01 17:17:58 +0600

    search and song home fixed

--- server/routes/songs.js
+++ server/routes/songs.js
@@ -10,10 +10,10 @@ const db = require('../db');
 
 router.get("/", async (req, res) => {
     try {
-        const { search, sortBy = 'popularity', sortOrder = 'desc', artist, genre } = req.query;
+        const { search, sortBy, sortOrder, artist, genre } = req.query;
         const queryParams = [];
         let query = `
-            SELECT S.*, A.ARTIST_NAME, AL.ALBUM_NAME, G.GENRE_NAME 
+            SELECT *
             FROM SONG S
             JOIN ARTIST A ON S.ARTIST_ID = A.ARTIST_ID
             JOIN ALBUM AL ON S.ALBUM_ID = AL.ALBUM_ID
@@ -34,15 +34,13 @@ router.get("/", async (req, res) => {
         }
 
         let orderByClause = '';
-        if (sortBy === 'popularity') {
-            orderByClause = `popularity ${sortOrder.toUpperCase()}`;
-        } else if (sortBy === 'price') {
-            orderByClause = `price ${sortOrder.toUpperCase()}`;
-        } else {
-            orderByClause = `${sortBy} ${sortOrder.toUpperCase()}, popularity DESC`;
+        if (sortBy) {
+            query += ` ORDER BY S.${sortBy.toUpperCase()}`;
+        }
+        if (sortOrder) {
+            query += ` ORDER BY ${orderByClause.toUpperCase()}`;
         }
 
-        query += ` ORDER BY ${orderByClause}`;
 
         const results = await db.query(query, queryParams);
         res.status(200).json({
@@ -64,17 +62,51 @@ router.get("/:song_id", async (req, res) => {
     try {
         const song_id = req.params.song_id;
         const query = `SELECT 
-        S.NAME, S.SONG_LENGTH, S.POPULARITY, A.ARTIST_NAME, AL.ALBUM_NAME, G.GENRE_NAME, 
-        P.PLATFORM_NAME, RT.RECTYPE_NAME        
-        FROM SONG S
-        JOIN ARTIST A ON S.ARTIST_ID = A.ARTIST_ID
-        JOIN ALBUM AL ON S.ALBUM_ID = AL.ALBUM_ID
-        JOIN GENRE G ON S.GENRE_ID = G.GENRE_ID
-        JOIN PLATFORM_SONG PS ON S.SONG_ID = PS.SONG_ID
-        JOIN RECORDING_SONG RTS ON RTS.SONG_ID = S.SONG_ID
-        JOIN PLATFORM P ON PS.PLATFORM_ID = P.PLATFORM_ID
-        JOIN REC_TYPE RT ON RTS.RECTYPE_ID = RT.RECTYPE_ID
-        WHERE S.song_id = $1`;
+        S.song_id AS song_id,
+        S.name AS song_name,
+        S.album_id, S.age_rating, S.popularity, S.price, S.genre_id, S.artist_id,
+        AL.album_id AS album_id,
+        AL.album_name AS album_name,
+        AL.album_year,G.genre_name,
+        G.genre_name AS genre_name,
+        PS.platform_id AS platform_id,
+        P.platform_name AS platform_name,
+        P.total_visit AS total_visits,
+        RT.rectype_name AS rectype_name,
+        RT.rectype_id AS rectype_id,
+        SS.synopsis AS synopsis,
+        P1.person_id AS composer_id,
+        P1.name AS composer_name,
+        P1.gender AS composer_gender,
+        P1.nationality AS composer_nationality,
+        P1.biography AS composer_biography,
+        P2.person_id AS lyricist_id,
+        P2.name AS lyricist_name,
+        P2.gender AS lyricist_gender,
+        P2.nationality AS lyricist_nationality,
+        P2.biography AS lyricist_biography,
+        P3.person_id AS producer_id,
+        P3.name AS producer_name,
+        P3.nationality AS producer_nationality,
+        P3.gender AS producer_gender,
+        P3.biography AS producer_biography
+    FROM SONG S
+    LEFT JOIN ARTIST A ON S.ARTIST_ID = A.ARTIST_ID
+    LEFT JOIN ALBUM AL ON S.ALBUM_ID = AL.ALBUM_ID
+    LEFT JOIN GENRE G ON S.GENRE_ID = G.GENRE_ID
+    LEFT JOIN PLATFORM_SONG PS ON S.SONG_ID = PS.SONG_ID
+    LEFT JOIN PLATFORM P ON PS.PLATFORM_ID = P.PLATFORM_ID
+    LEFT JOIN RECORDING_SONG RS ON S.SONG_ID = RS.SONG_ID
+    LEFT JOIN REC_TYPE RT ON RS.RECTYPE_ID = RT.RECTYPE_ID
+    LEFT JOIN SONG_SYNOPSIS SS ON S.SONG_ID = SS.SONG_ID
+    LEFT JOIN COMPOSER C ON S.SONG_ID = C.SONG_ID
+    LEFT JOIN LYRICIST L ON S.SONG_ID = L.SONG_ID
+    LEFT JOIN PRODUCER PR ON S.SONG_ID = PR.SONG_ID
+    LEFT JOIN PEOPLE P1 ON C.COMPOSER_ID = P1.PERSON_ID
+    LEFT JOIN PEOPLE P2 ON L.LYRICIST_ID = P2.PERSON_ID
+    LEFT JOIN PEOPLE P3 ON PR.PRODUCER_ID = P3.PERSON_ID
+    WHERE S.song_id = $1;
+    `;
 
         const result = await db.query(query, [song_id]);
         res.status(200).json({
--- sql/Artist.sql
+++ sql/Artist.sql
@@ -1,88 +1,400 @@
 CREATE TABLE artist (
     artist_id BIGSERIAL NOT NULL PRIMARY KEY,
     artist_name VARCHAR(100),
-    album_id INTEGER,
-    review_id INTEGER,
     alias VARCHAR(100),
     small_biography TEXT
 );
 
-INSERT INTO artist (artist_name, album_id, review_id, alias, small_biography) VALUES
-    ('Aretha Franklin', random(), random(), 'Queen of Soul', 'Legendary singer known for her powerful voice.'),
-    ('Chuck Berry', random(), random(), 'Father of Rock and Roll', 'Pioneer of rock music and iconic guitarist.'),
-    ('Michael Jackson', random(), random(), 'King of Pop', 'Global pop sensation and innovative entertainer.'),
-    ('Bill Haley & His Comets', random(), random(), 'Rock and Roll Trailblazers', 'Early rock and roll pioneers.'),
-    ('Led Zeppelin', random(), random(), 'Legendary Rock Band', 'Known for their iconic rock anthems.'),
-    ('The Kingsmen', random(), random(), 'Garage Rock Pioneers', 'Famous for the hit song  Louie Louie .'),
-    ('Marvin Gaye', random(), random(), 'Prince of Soul', 'Smooth and soulful vocalist with social consciousness.'),
-    ('The Rolling Stones', random(), random(), 'Rock Legends', 'Long-standing rock band with numerous hits.'),
-    ('Elvis Presley', random(), random(), 'King of Rock and Roll', 'Iconic figure in the history of rock music.'),
-    ('Bob Dylan', random(), random(), 'Voice of a Generation', 'Nobel Prize-winning singer-songwriter.'),
-    ('The Beatles', random(), random(), 'Fab Four', 'Revolutionized music and pop culture.'),
-    ('The Beach Boys', random(), random(), 'Harmony Masters', 'Influential for their vocal harmonies and beach-themed music.'),
-    ('Little Richard', random(), random(), 'Architect of Rock and Roll', 'Energetic and flamboyant rock and roll pioneer.'),
-    ('Ray Charles', random(), random(), 'Genius of Soul', 'Blind pianist and soulful singer.'),
-    ('Eagles', random(), random(), 'Rock Superstars', 'Famous for their harmonious sound and hit songs.'),
-    ('The Righteous Brothers', random(), random(), 'Blue-Eyed Soul', 'Known for the emotional ballad  Youve Lost That Lovin Feelin'),
-    ('The Temptations', random(), random(), 'Motown Legends', 'Smooth vocal group with soulful hits.'),
-    ('John Lennon', random(), random(), 'Imagine', 'Co-founder of The Beatles and influential solo artist.'),
-    ('Nirvana', random(), random(), 'Grunge Pioneers', 'Led by Kurt Cobain, influential in the grunge movement.'),
-    ('Bruce Springsteen', random(), random(), 'The Boss', 'Energetic rock performer and songwriter.'),
-    ('The Doors', random(), random(), 'Psychedelic Rock', 'Known for their poetic lyrics and unique sound.'),
-    ('Stevie Wonder', random(), random(), 'Musical Prodigy', 'Blind since birth, multi-instrumentalist and soulful singer.'),
-    ('Derek and the Dominos (Eric Clapton)', random(), random(), 'Blues-Rock Supergroup', 'Led by Eric Clapton, famous for Layla .'),
-    ('Queen', random(), random(), 'Champions', 'Legendary rock band with diverse musical styles.'),
-    ('Eddie Cochran', random(), random(), 'Rockabilly Star', 'Influential rock and roll and rockabilly artist.'),
-    ('The Kinks', random(), random(), 'British Invasion', 'Known for their distinctive sound and witty lyrics.'),
-    ('Simon & Garfunkel', random(), random(), 'Folk Rock Duo', 'Harmonious duo with poetic and introspective songs.'),
-    ('Al Green', random(), random(), 'The Reverend', 'Smooth soul singer and ordained pastor.'),
-    ('Jimi Hendrix', random(), random(), 'Guitar Virtuoso', 'Innovative guitarist and rock icon.'),
-    ('Jerry Lee Lewis', random(), random(), 'The Killer', 'Energetic and flamboyant rock and roll pioneer.'),
-    ('Buddy Holly', random(), random(), 'Rockabilly Legend', 'Influential figure in the early days of rock and roll.'),
-    ('The Police', random(), random(), 'New Wave Icons', 'Sting-led band with a blend of rock and new wave.'),
-    ('OutKast', random(), random(), 'Hip-Hop Duo', 'Andre 3000 and Big Boi, known for their genre-blending music.'),
-    ('Bo Diddley', random(), random(), 'Bo Diddley Beat', 'Innovative guitarist and rhythm pioneer.'),
-    ('James Brown', random(), random(), 'Godfather of Soul', 'The hardest-working man in show business.'),
-    ('Prince', random(), random(), 'Purple One', 'Musical genius and innovative artist.'),
-    ('The Who', random(), random(), 'Rock Legends', 'Known for their energetic performances and concept albums.'),
-    ('R.E.M.', random(), random(), 'Alternative Pioneers', 'Influential in the alternative rock genre.'),
-    ('Cream', random(), random(), 'Supergroup', 'Eric Clapton, Ginger Baker, and Jack Bruce in a rock supergroup.'),
-    ('U2', random(), random(), 'Irish Rock Icons', 'Led by Bono, known for their anthemic rock songs.'),
-    ('Sam Cooke', random(), random(), 'Soulful Crooner', 'One of the greatest soul and R&B singers.'),
-    ('Grandmaster Flash and the Furious Five', random(), random(), 'Hip-Hop Pioneers', 'Influential in the early days of hip-hop.'),
-    ('Wilson Pickett', random(), random(), 'Wicked Pickett', 'Soulful singer known for his powerful voice.'),
-    ('The Byrds', random(), random(), 'Folk Rock Pioneers', 'Known for blending folk and rock elements.'),
-    ('Creedence Clearwater Revival', random(), random(), 'Swamp Rock', 'Led by John Fogerty, known for their swamp rock sound.'),
-    ('Big Joe Turner', random(), random(), 'Boss of the Blues', 'Blues and R&B singer with a powerful voice.'),
-    ('Aerosmith', random(), random(), 'Bad Boys from Boston', 'Hard rock band with a string of hits.'),
-    ('Rod Stewart', random(), random(), 'Rod the Mod', 'Versatile rocker with a raspy voice.'),
-    ('Don McLean', random(), random(), 'American Pie', 'Folk singer-songwriter known for the iconic song.'),
-    ('Otis Redding', random(), random(), 'King of Soul', 'Influential soul singer with a raw emotive style.'),
-    ('Bee Gees', random(), random(), 'Disco Legends', 'Famous for their contributions to the disco era.'),
-    ('Lynyrd Skynyrd', random(), random(), 'Southern Rock Pioneers', 'Known for  Free Bird  and  Sweet Home Alabama .'),
-    ('Dr. Dre', random(), random(), 'Hip-Hop Mogul', 'Legendary rapper, producer,'),
-	('Percy Sledge', random(), random(), 'Soulful Balladeer', 'Known for the classic  When a Man Loves a Woman .'),
-    ('Ben E. King', random(), random(), 'Stand By Me', 'R&B and soul singer, famous for the timeless hit  Stand By Me .'),
-    ('Gloria Gaynor', random(), random(), 'Queen of Disco', 'Iconic disco singer, best known for  I Will Survive .'),
-    ('Rick James', random(), random(), 'Super Freak', 'Funky and flamboyant artist, famous for  Super Freak .'),
-    ('Procol Harum', random(), random(), 'Progressive Rock', 'Known for the classic  A Whiter Shade of Pale .'),
-    ('The Ronettes', random(), random(), 'Girl Group', 'Legendary girl group, famous for  Be My Baby .'),
-    ('Fleetwood Mac', random(), random(), 'Rock Legends', 'British-American rock band with a string of hits.'),
-    ('The Animals', random(), random(), 'British Invasion', 'Rock band known for hits like  House of the Rising Sun .'),
-    ('The Supremes', random(), random(), 'Motown Royalty', 'One of Motowns most successful acts with numerous chart-topping hits.'),
-    ('Sugarhill Gang', random(), random(), 'Hip-Hop Pioneers', 'Early hip-hop group known for the groundbreaking  Rappers Delight .'),
-    ('The Jackson 5', random(), random(), 'Motown Sensation', 'Childhood group of Michael Jackson, known for hits like  I Want You Back .'),
-    ('Joan Jett & The Blackhearts', random(), random(), 'Rock Goddess', 'Punk rock and rock artist, famous for  I Love Rock N Roll .'),
-    ('The Human League', random(), random(), 'Synth-Pop Pioneers', 'Electropop band known for hits like  Dont You Want Me .'),
-    ('Pearl Jam', random(), random(), 'Grunge Icons', 'Influential grunge band with hits like  Jeremy  and  Alive .'),
-    ('Madonna', random(), random(), 'Queen of Pop', 'Iconic pop singer, actress, and cultural influencer.'),
-    ('Guns N  Roses', random(), random(), 'Rock Icons', 'Hard rock band with hits like  Sweet Child O Mine  and  November Rain .'),
-    ('Red Hot Chili Peppers', random(), random(), 'Funky Rockers', 'Alternative rock band with a fusion of funk and rock elements.'),
-    ('Elton John', random(), random(), 'Rocket Man', 'Legendary singer-songwriter and pianist with a vast catalog of hits.'),
-    ('ABBA', random(), random(), 'Swedish Pop Sensation', 'One of the most successful pop groups in history, known for  Dancing Queen .'),
-    ('Journey', random(), random(), 'Arena Rock Legends', 'Rock band with anthemic hits like  Dont Stop Believin.'),
-    ('Sam & Dave', random(), random(), 'Soul Duo', 'Soulful duo known for hits like  Soul Man  and  Hold On, Im Comin .'),
-    ('AC/DC', random(), random(), 'Rock or Bust', 'Legendary rock band known for their electrifying performances.'),
-    ('The Everly Brothers', random(), random(), 'Harmony Kings', 'Influential duo known for their close harmonies and rockabilly sound.'),
-    ('The Penguins', random(), random(), 'Doo-Wop Legends', 'Doo-wop group famous for the classic  Earth Angel .'),
-    ('Patsy Cline', random(), random(), 'Queen of Country', 'Country music icon known for her timeless songs and emotional performances.');
+INSERT INTO
+    artist (artist_name, alias, small_biography)
+VALUES
+    (
+        'Aretha Franklin',
+        'Queen of Soul',
+        'Legendary singer known for her powerful voice.'
+    ),
+    (
+        'Chuck Berry',
+        'Father of Rock and Roll',
+        'Pioneer of rock music and iconic guitarist.'
+    ),
+    (
+        'Michael Jackson',
+        'King of Pop',
+        'Global pop sensation and innovative entertainer.'
+    ),
+    (
+        'Bill Haley & His Comets',
+        'Rock and Roll Trailblazers',
+        'Early rock and roll pioneers.'
+    ),
+    (
+        'Led Zeppelin',
+        'Legendary Rock Band',
+        'Known for their iconic rock anthems.'
+    ),
+    (
+        'The Kingsmen',
+        'Garage Rock Pioneers',
+        'Famous for the hit song  Louie Louie .'
+    ),
+    (
+        'Marvin Gaye',
+        'Prince of Soul',
+        'Smooth and soulful vocalist with social consciousness.'
+    ),
+    (
+        'The Rolling Stones',
+        'Rock Legends',
+        'Long-standing rock band with numerous hits.'
+    ),
+    (
+        'Elvis Presley',
+        'King of Rock and Roll',
+        'Iconic figure in the history of rock music.'
+    ),
+    (
+        'Bob Dylan',
+        'Voice of a Generation',
+        'Nobel Prize-winning singer-songwriter.'
+    ),
+    (
+        'The Beatles',
+        'Fab Four',
+        'Revolutionized music and pop culture.'
+    ),
+    (
+        'The Beach Boys',
+        'Harmony Masters',
+        'Influential for their vocal harmonies and beach-themed music.'
+    ),
+    (
+        'Little Richard',
+        'Architect of Rock and Roll',
+        'Energetic and flamboyant rock and roll pioneer.'
+    ),
+    (
+        'Ray Charles',
+        'Genius of Soul',
+        'Blind pianist and soulful singer.'
+    ),
+    (
+        'Eagles',
+        'Rock Superstars',
+        'Famous for their harmonious sound and hit songs.'
+    ),
+    (
+        'The Righteous Brothers',
+        'Blue-Eyed Soul',
+        'Known for the emotional ballad  Youve Lost That Lovin Feelin'
+    ),
+    (
+        'The Temptations',
+        'Motown Legends',
+        'Smooth vocal group with soulful hits.'
+    ),
+    (
+        'John Lennon',
+        'Imagine',
+        'Co-founder of The Beatles and influential solo artist.'
+    ),
+    (
+        'Nirvana',
+        'Grunge Pioneers',
+        'Led by Kurt Cobain, influential in the grunge movement.'
+    ),
+    (
+        'Bruce Springsteen',
+        'The Boss',
+        'Energetic rock performer and songwriter.'
+    ),
+    (
+        'The Doors',
+        'Psychedelic Rock',
+        'Known for their poetic lyrics and unique sound.'
+    ),
+    (
+        'Stevie Wonder',
+        'Musical Prodigy',
+        'Blind since birth, multi-instrumentalist and soulful singer.'
+    ),
+    (
+        'Derek and the Dominos (Eric Clapton)',
+        'Blues-Rock Supergroup',
+        'Led by Eric Clapton, famous for Layla .'
+    ),
+    (
+        'Queen',
+        'Champions',
+        'Legendary rock band with diverse musical styles.'
+    ),
+    (
+        'Eddie Cochran',
+        'Rockabilly Star',
+        'Influential rock and roll and rockabilly artist.'
+    ),
+    (
+        'The Kinks',
+        'British Invasion',
+        'Known for their distinctive sound and witty lyrics.'
+    ),
+    (
+        'Simon & Garfunkel',
+        'Folk Rock Duo',
+        'Harmonious duo with poetic and introspective songs.'
+    ),
+    (
+        'Al Green',
+        'The Reverend',
+        'Smooth soul singer and ordained pastor.'
+    ),
+    (
+        'Jimi Hendrix',
+        'Guitar Virtuoso',
+        'Innovative guitarist and rock icon.'
+    ),
+    (
+        'Jerry Lee Lewis',
+        'The Killer',
+        'Energetic and flamboyant rock and roll pioneer.'
+    ),
+    (
+        'Buddy Holly',
+        'Rockabilly Legend',
+        'Influential figure in the early days of rock and roll.'
+    ),
+    (
+        'The Police',
+        'New Wave Icons',
+        'Sting-led band with a blend of rock and new wave.'
+    ),
+    (
+        'OutKast',
+        'Hip-Hop Duo',
+        'Andre 3000 and Big Boi, known for their genre-blending music.'
+    ),
+    (
+        'Bo Diddley',
+        'Bo Diddley Beat',
+        'Innovative guitarist and rhythm pioneer.'
+    ),
+    (
+        'James Brown',
+        'Godfather of Soul',
+        'The hardest-working man in show business.'
+    ),
+    (
+        'Prince',
+        'Purple One',
+        'Musical genius and innovative artist.'
+    ),
+    (
+        'The Who',
+        'Rock Legends',
+        'Known for their energetic performances and concept albums.'
+    ),
+    (
+        'R.E.M.',
+        'Alternative Pioneers',
+        'Influential in the alternative rock genre.'
+    ),
+    (
+        'Cream',
+        'Supergroup',
+        'Eric Clapton, Ginger Baker, and Jack Bruce in a rock supergroup.'
+    ),
+    (
+        'U2',
+        'Irish Rock Icons',
+        'Led by Bono, known for their anthemic rock songs.'
+    ),
+    (
+        'Sam Cooke',
+        'Soulful Crooner',
+        'One of the greatest soul and R&B singers.'
+    ),
+    (
+        'Grandmaster Flash and the Furious Five',
+        'Hip-Hop Pioneers',
+        'Influential in the early days of hip-hop.'
+    ),
+    (
+        'Wilson Pickett',
+        'Wicked Pickett',
+        'Soulful singer known for his powerful voice.'
+    ),
+    (
+        'The Byrds',
+        'Folk Rock Pioneers',
+        'Known for blending folk and rock elements.'
+    ),
+    (
+        'Creedence Clearwater Revival',
+        'Swamp Rock',
+        'Led by John Fogerty, known for their swamp rock sound.'
+    ),
+    (
+        'Big Joe Turner',
+        'Boss of the Blues',
+        'Blues and R&B singer with a powerful voice.'
+    ),
+    (
+        'Aerosmith',
+        'Bad Boys from Boston',
+        'Hard rock band with a string of hits.'
+    ),
+    (
+        'Rod Stewart',
+        'Rod the Mod',
+        'Versatile rocker with a raspy voice.'
+    ),
+    (
+        'Don McLean',
+        'American Pie',
+        'Folk singer-songwriter known for the iconic song.'
+    ),
+    (
+        'Otis Redding',
+        'King of Soul',
+        'Influential soul singer with a raw emotive style.'
+    ),
+    (
+        'Bee Gees',
+        'Disco Legends',
+        'Famous for their contributions to the disco era.'
+    ),
+    (
+        'Lynyrd Skynyrd',
+        'Southern Rock Pioneers',
+        'Known for  Free Bird  and  Sweet Home Alabama .'
+    ),
+    (
+        'Dr. Dre',
+        'Hip-Hop Mogul',
+        'Legendary rapper, producer,'
+    ),
+    (
+        'Percy Sledge',
+        'Soulful Balladeer',
+        'Known for the classic  When a Man Loves a Woman .'
+    ),
+    (
+        'Ben E. King',
+        'Stand By Me',
+        'R&B and soul singer, famous for the timeless hit  Stand By Me .'
+    ),
+    (
+        'Gloria Gaynor',
+        'Queen of Disco',
+        'Iconic disco singer, best known for  I Will Survive .'
+    ),
+    (
+        'Rick James',
+        'Super Freak',
+        'Funky and flamboyant artist, famous for  Super Freak .'
+    ),
+    (
+        'Procol Harum',
+        'Progressive Rock',
+        'Known for the classic  A Whiter Shade of Pale .'
+    ),
+    (
+        'The Ronettes',
+        'Girl Group',
+        'Legendary girl group, famous for  Be My Baby .'
+    ),
+    (
+        'Fleetwood Mac',
+        'Rock Legends',
+        'British-American rock band with a string of hits.'
+    ),
+    (
+        'The Animals',
+        'British Invasion',
+        'Rock band known for hits like  House of the Rising Sun .'
+    ),
+    (
+        'The Supremes',
+        'Motown Royalty',
+        'One of Motowns most successful acts with numerous chart-topping hits.'
+    ),
+    (
+        'Sugarhill Gang',
+        'Hip-Hop Pioneers',
+        'Early hip-hop group known for the groundbreaking  Rappers Delight .'
+    ),
+    (
+        'The Jackson 5',
+        'Motown Sensation',
+        'Childhood group of Michael Jackson, known for hits like  I Want You Back .'
+    ),
+    (
+        'Joan Jett & The Blackhearts',
+        'Rock Goddess',
+        'Punk rock and rock artist, famous for  I Love Rock N Roll .'
+    ),
+    (
+        'The Human League',
+        'Synth-Pop Pioneers',
+        'Electropop band known for hits like  Dont You Want Me .'
+    ),
+    (
+        'Pearl Jam',
+        'Grunge Icons',
+        'Influential grunge band with hits like  Jeremy  and  Alive .'
+    ),
+    (
+        'Madonna',
+        'Queen of Pop',
+        'Iconic pop singer, actress, and cultural influencer.'
+    ),
+    (
+        'Guns N  Roses',
+        'Rock Icons',
+        'Hard rock band with hits like  Sweet Child O Mine  and  November Rain .'
+    ),
+    (
+        'Red Hot Chili Peppers',
+        'Funky Rockers',
+        'Alternative rock band with a fusion of funk and rock elements.'
+    ),
+    (
+        'Elton John',
+        'Rocket Man',
+        'Legendary singer-songwriter and pianist with a vast catalog of hits.'
+    ),
+    (
+        'ABBA',
+        'Swedish Pop Sensation',
+        'One of the most successful pop groups in history, known for  Dancing Queen .'
+    ),
+    (
+        'Journey',
+        'Arena Rock Legends',
+        'Rock band with anthemic hits like  Dont Stop Believin.'
+    ),
+    (
+        'Sam & Dave',
+        'Soul Duo',
+        'Soulful duo known for hits like  Soul Man  and  Hold On, Im Comin .'
+    ),
+    (
+        'AC/DC',
+        'Rock or Bust',
+        'Legendary rock band known for their electrifying performances.'
+    ),
+    (
+        'The Everly Brothers',
+        'Harmony Kings',
+        'Influential duo known for their close harmonies and rockabilly sound.'
+    ),
+    (
+        'The Penguins',
+        'Doo-Wop Legends',
+        'Doo-wop group famous for the classic  Earth Angel .'
+    ),
+    (
+        'Patsy Cline',
+        'Queen of Country',
+        'Country music icon known for her timeless songs and emotional performances.'
+    );
