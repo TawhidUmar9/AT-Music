88deddae34200c9715945e9d878c5f99d3c13297
Tawhid <tawhidumar.21@protonmail.com>
2024-03-02 00:58:11 +0600

    add song completed

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const db = require('../db');
 
 
+const addSongRoute = require('./addsong');
+router.use('/', addSongRoute);
+
 
 router.get("/", async (req, res) => {
     try {
@@ -259,127 +262,6 @@ router.post("/genre", async (req, res) => {
         res.status(500).json({
             status: "error",
             message: "Failed to add Genre"
-        });
-    }
-});
-//add song
-router.post("/song", async (req, res) => {
-    try {
-        const { song_name, artist_name, album_name, age_rating, genre_name, price,
-            recording_type, platform_type, song_length } = req.body;
-        if (!song_name || !artist_name || !album_name || !age_rating || !genre_name || !price
-            || !recording_type || !platform_type || !song_length) {
-            return res.status(400).json({
-                status: "error",
-                message: "Song name, artist name, " +
-                    "album name, age rating, genre name, and price are required"
-            });
-        }
-
-        const songCheckQuery = `SELECT * FROM song WHERE LOWER(name) = LOWER($1)`;
-        const songCheckResult = await db.query(songCheckQuery, [song_name]);
-
-        if (songCheckResult.rows.length > 0) {
-            return res.status(400).json({
-                status: "error",
-                message: "Song already exists"
-            });
-        }
-
-        // Find artist_id from artist table
-        const artistQuery = `SELECT artist_id FROM artist WHERE LOWER(artist_name) = LOWER($1)`;
-        const artistResult = await db.query(artistQuery, [artist_name]);
-        // Check if artist exists
-        if (artistResult.rows.length === 0) {
-            return res.status(404).json({
-                status: "error",
-                message: "Artist not found"
-            });
-        }
-        const artist_id = artistResult.rows[0].artist_id;
-
-        // Find album_id from album table
-        const albumQuery = `SELECT album_id FROM album WHERE LOWER(album_name) = LOWER($1)`;
-        const albumResult = await db.query(albumQuery, [album_name]);
-        // Check if album exists
-        if (albumResult.rows.length === 0) {
-            return res.status(404).json({
-                status: "error",
-                message: "Album not found"
-            });
-        }
-        const album_id = albumResult.rows[0].album_id;
-
-        // Find genre_id from genre table
-        const genreQuery = `SELECT genre_id FROM genre WHERE LOWER(genre_name) = LOWER($1)`;
-        const genreResult = await db.query(genreQuery, [genre_name]);
-        // Check if genre exists
-        if (genreResult.rows.length === 0) {
-            return res.status(404).json({
-                status: "error",
-                message: "Genre not found"
-            });
-        }
-        const genre_id = genreResult.rows[0].genre_id;
-
-        // Find platform_id from platform table
-        const platformQuery = `SELECT platform_id FROM
-                    platform WHERE LOWER(platform_name) = LOWER($1)`;
-        const platformResult = await db.query(platformQuery, [platform_type]);
-        // Check if platform exists
-        if (platformResult.rows.length === 0) {
-            return res.status(404).json({
-                status: "error",
-                message: "Platform not found"
-            });
-        }
-        const platform_id = platformResult.rows[0].platform_id;
-
-        // Find recording_id from recording table
-        const recordingQuery = `SELECT rectype_id FROM 
-                    rec_type WHERE LOWER(rectype_name) = LOWER($1)`;
-        const recordingResult = await db.query(recordingQuery, [recording_type]);
-        // Check if recording exists
-        if (recordingResult.rows.length === 0) {
-            return res.status(404).json({
-                status: "error",
-                message: "Recording not found"
-            });
-        }
-        const recording_id = recordingResult.rows[0].recording_id;
-
-        // Insert the song into the database
-        const query = `INSERT INTO song (artist_id, name, album_id, song_length,
-                        age_rating, genre_id, price) 
-                       VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`;
-
-        const result = await db.query(query, [artist_id, song_name, album_id, song_length,
-            age_rating, genre_id, price]);
-
-        const songQuery = `SELECT song_id FROM song WHERE LOWER(name) = LOWER($1)`;
-        const songResult = await db.query(songQuery, [song_name]);
-        const song_id = songResult.rows[0].song_id;
-
-        const songPlatformQuery = `INSERT INTO platfrom_song (song_id, platform_id)
-                                    VALUES ($1, $2)`;
-        await db.query(songPlatformQuery, [song_id, platform_id]);
-
-        const songRecordingQuery = `INSERT INTO recording_song (song_id, recording_id)
-                                    VALUES ($1, $2)`;
-        await db.query(songRecordingQuery, [song_id, recording_id]);
-
-
-        res.status(201).json({
-            status: "success",
-            message: `Song '${song_name}' added successfully`,
-            data: result.rows[0]
-        });
-
-    } catch (error) {
-        console.error("Error:", error);
-        return res.status(500).json({
-            status: "error",
-            message: "Internal server error"
         });
     }
 });
