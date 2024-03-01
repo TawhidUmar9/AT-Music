e01ec42f21cf663316e916ec415c9fa54c8681d6
Tawhid <tawhidumar.21@protonmail.com>
2024-03-01 18:04:32 +0600

    getting user likes updated, awards feature added

--- server/routes/user.js
+++ server/routes/user.js
@@ -504,10 +504,58 @@ router.post("/add/playlist/:playlist_name", async (req, res) => {
     }
 });
 
+//get liked entities
+router.get("/like", async (req, res) => {
+    try {
+        const { user_id } = req.body;
+        if (!user_id) {
+            return res.status(400).json({
+                status: "error",
+                message: "User ID and Song ID are required"
+            });
+        }
+        const query = `
+        SELECT * 
+        FROM liked_song L
+        JOIN SONG S ON S.SONG_ID = L.SONG_ID
+        WHERE user_id = $1`;
+        const liked_song = await db.query(query, [user_id]);
 
+        const query2 = `
+        SELECT * 
+        FROM liked_artist L
+        JOIN ARTIST A ON A.ARTIST_ID = L.ARTIST_ID
+        WHERE user_id = $1`;
+        const liked_artist = await db.query(query2, [user_id]);
 
+        const query3 = `
+        SELECT * 
+        FROM liked_album L
+        JOIN ALBUM AL ON AL.ALBUM_ID = L.ALBUM_ID
+        WHERE user_id = $1`;
+        const liked_album = await db.query(query3, [user_id]);
 
+        const query4 = `
+        SELECT * 
+        FROM liked_genre L
+        JOIN GENRE G ON G.GENRE_ID = L.GENRE_ID
+        WHERE user_id = $1`;
+        const liked_genre = await db.query(query4, [user_id]);
 
-
+        res.status(200).json({
+            status: "success",
+            liked_song: liked_song.rows,
+            liked_artist: liked_artist.rows,
+            liked_album: liked_album.rows,
+            liked_genre: liked_genre.rows
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            status: "error",
+            message: "Internal Server Error"
+        });
+    }
+});
 
 module.exports = router;
